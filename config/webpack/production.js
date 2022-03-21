3fc585c8de3c5f170d1e808606b58714d7a4f099
Oscar M Romero <omromero@CodePC.lan>
2022-03-21 17:29:34 -0400

    updated database.yml username, config/production https to http, updated webpacl/production  copied envrioment.js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,15 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+
+const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
+
 module.exports = environment.toWebpackConfig()
